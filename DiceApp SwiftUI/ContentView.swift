96b1a589dedc8f7e02f043b14de408e30c1a235c
lpereira <lpereira@wgod-alp-mac172.local>
2022-11-10 09:05:58 -0300

    inicial

---
+++ DiceApp SwiftUI/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  DiceApp SwiftUI
+//
+//  Created by lpereira on 10/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 2
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }, label: {
+                    Text("Roll")
+                        .font(.system(size: 35))
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                })
+                    .background(Color.blue)
+                    .cornerRadius(15)
+            }.padding(.horizontal)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct DiceView: View {
+    var n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding(.all)
+    }
+}
---
+++ DiceApp SwiftUI/DiceApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp_SwiftUIApp.swift
+//  DiceApp SwiftUI
+//
+//  Created by lpereira on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
